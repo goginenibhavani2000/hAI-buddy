e320dcb85f09a9feca1f317d8ca0dafe8cbf313b
GreeshmaJasti <greeshmajasti2000@gmail.com>
2026-02-28 22:37:06 -0500

    updated api url

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,1 +1,2 @@
-export const API_URL='https://hai-buddy.onrender.com/' || 'http://localhost:8000/';
+// export const API_URL='http://localhost:8000';
+export const API_URL='https://your-fastapi-backend-url.com';
